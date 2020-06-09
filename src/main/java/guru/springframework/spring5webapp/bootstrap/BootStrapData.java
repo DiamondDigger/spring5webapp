341f26a60daa0384dbe640640cf67eb58f0db853
naglec13 <naglec13@gmail.com>
2020-06-09 10:52:34 +0300

    ..setting up relationship between publisher and books

--- src/main/java/guru/springframework/spring5webapp/bootstrap/BootStrapData.java
+++ src/main/java/guru/springframework/spring5webapp/bootstrap/BootStrapData.java
@@ -25,31 +25,39 @@ public class BootStrapData implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
 
-        Author ron = new Author("Ron", "Jameson");
-        Book BoP = new Book("Bible of Programming", "4352398");
-
-        ron.getBooks().add(BoP);
-        BoP.getAuthors().add(ron);
-
-        authorRepository.save(ron);
-        bookRepository.save(BoP);
-
-        Author mack = new Author("Mack", "Pot");
-        Book JVM = new Book("Java Virtual Machine", "12340458");
-
-        ron.getBooks().add(JVM);
-        BoP.getAuthors().add(mack);
-
-        authorRepository.save(mack);
-        bookRepository.save(JVM);
 
         Publisher russianPublisher = new Publisher("SouzDruk", "Red 21", "Moscow",
                 "Virginia", "23415");
         publisherRepository.save(russianPublisher);
 
+        Author ron = new Author("Ron", "Jameson");
+        Book BoP = new Book("Bible of Programming", "4352398");
+
+        ron.getBooks().add(BoP);
+        BoP.getAuthors().add(ron);
+        BoP.setPublisher(russianPublisher);
+        russianPublisher.getBooks().add(BoP);
+
+        authorRepository.save(ron);
+        bookRepository.save(BoP);
+        publisherRepository.save(russianPublisher);
+
+        Author mack = new Author("Mack", "Pot");
+        Book JVM = new Book("Java Virtual Machine", "12340458");
+
+        mack.getBooks().add(JVM);
+        JVM.getAuthors().add(mack);
+        JVM.setPublisher(russianPublisher);
+        russianPublisher.getBooks().add(JVM);
+
+        authorRepository.save(mack);
+        bookRepository.save(JVM);
+        publisherRepository.save(russianPublisher);
+
         System.out.println("BootStrap is starting....");
         System.out.println("bookRepository from " + bookRepository.count() + " book(s)");
         System.out.println("publisherRepository from " + publisherRepository.count() + " publisher(s)");
-        System.out.println("russianPublisher: "+russianPublisher.toString());
+        System.out.println("russianPublisher.getBooks = " + russianPublisher.getBooks().size());
+        System.out.println("Details about publisher: " + russianPublisher.toString());
     }
 }
